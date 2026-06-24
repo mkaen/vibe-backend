35e40677b399493ac71cd90b84c2c60b8cd44fef
Margus Käen <margus.kaen@gmail.com>
2026-06-24 23:13:45 +0300

    modified register endpoint and fixed status_code response

--- src/features/auth/router.py
+++ src/features/auth/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.features.auth.schemas import LoginRequest, RegisterRequest
+from fastapi.responses import JSONResponse
 
 auth_router_v1 = APIRouter(prefix="/v1/auth", tags=["auth"])
 
@@ -7,7 +8,7 @@ auth_router_v1 = APIRouter(prefix="/v1/auth", tags=["auth"])
 @auth_router_v1.post("/login", tags=["auth"])
 async def login(login_request: LoginRequest):
     print(f"Login request received: {login_request.email} {login_request.password}")
-    return {"message": "Login successful!"}, 200
+    return JSONResponse(content={"message": "Login successful!"}, status_code=200)
 
 
 
@@ -15,4 +16,4 @@ async def login(login_request: LoginRequest):
 @auth_router_v1.post("/register", tags=["auth"])
 async def register(registration_data: RegisterRequest):
     print(f"Register request received: {registration_data}")
-    return {"message": "Register successful!"}, 201
+    return JSONResponse(content={"message": "Register successful!"}, status_code=201)
--- src/features/auth/schemas.py
+++ src/features/auth/schemas.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class LoginRequest(BaseModel):
     email: str
     password: str
 
 class RegisterRequest(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
-    phone_number: str
-    has_image: bool
+    first_name: str = Field(alias="firstName")
+    last_name: str = Field(alias="lastName")
+    phone: str
+    email: str = Field(alias="email")
+    password: str = Field(min_length=8)
+    has_image: bool = Field(alias="hasImage", default=False)
